9252551e2118e8211a7bab0f58e03a5cb9cf345a
Matteo Fortini <matteo.fortini@gmail.com>
2018-11-06 09:30:22 +0100

    Fix script conversione

--- _data/convertcsv.py
+++ _data/convertcsv.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-dd=pd.read_csv("Martiri e caduti - 1915-1918.csv")
+dd=pd.read_csv("Martiri e Caduti - 1915-1918.csv")
 dd["title"]=dd["Numero"].astype(str)+"_"+dd["Cognome"]+"_"+dd["Nome"]
 dd.to_json("caduti.json",orient="records")
