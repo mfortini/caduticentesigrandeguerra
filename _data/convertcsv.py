22a73b5e522c5228a25db412cd90b2f9b2148633
Matteo Fortini <matteo.fortini@gmail.com>
2021-05-16 11:33:00 +0200

    Aggiunta lista per cognome

--- _data/convertcsv.py
+++ _data/convertcsv.py
@@ -3,3 +3,7 @@ import pandas as pd
 dd=pd.read_csv("Martiri e Caduti - 1915-1918.csv")
 dd["title"]=dd["Numero"].astype(str)+"_"+dd["Cognome"]+"_"+dd["Nome"]
 dd.to_json("caduti.json",orient="records")
+
+cognomi=dd["Cognome"].unique().copy()
+cognomi.sort()
+pd.DataFrame({"Cognome":cognomi}).to_json("cognomi.json",orient="records")
